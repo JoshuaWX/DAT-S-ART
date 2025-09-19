bb9c6e7bfcbd83185612e1325109e94493cd444c
JoshuaWX <ejiyooye16571@run.edu.ng>
2025-09-19 11:25:15 +0100

    error 500 fix

--- api/cms-content.js
+++ api/cms-content.js
@@ -1,5 +1,5 @@
-const fs = require('fs').promises;
-const path = require('path');
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export default async function handler(req, res) {
     // Enable CORS
